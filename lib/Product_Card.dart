1888786ae86f22ab3f9ea4673d74ba55da3bd9be
Ridiska <temich04om@mail.ru>
2024-07-04 03:41:02 +0600

    Add state to ProductCard

--- lib/Product_Card.dart
+++ lib/Product_Card.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
 import 'product.dart';
 
-class ProductCard extends StatelessWidget {
+class ProductCard extends StatefulWidget {
   final Product product;
 
   const ProductCard({Key? key, required this.product}) : super(key: key);
+
+  @override
+  _ProductCardState createState() => _ProductCardState();
+}
+
+class _ProductCardState extends State<ProductCard> {
+  int quantity = 0;
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: Column(
         children: [
-          Image.network(product.imageUrl),
-          Text(product.name),
-          Text(product.price),
+          Image.network(widget.product.imageUrl),
+          Text(widget.product.name),
+          Text(widget.product.price),
         ],
       ),
     );
